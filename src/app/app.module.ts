e5d1f3c78cdabdf5427c61959ef81c1977d5eb57
jdsinniah <dario.sinniah@hotmail.it>
2020-03-14 23:55:27 +0100

    Initial commit (Added)
    Signed-off-by: jdsinniah <dario.sinniah@hotmail.it>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { RegistrationComponent } from './registration/registration.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
